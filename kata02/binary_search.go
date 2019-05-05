2b856c3ac93e6df6c83861d18dcd4a480acc6763
James Little <alphafoobar@users.noreply.github.com>
2019-05-05 22:45:03 +1200

    Simplify binary search algorithm

--- kata02/binary_search.go
+++ kata02/binary_search.go
@@ -1,36 +1,12 @@
 package kata02
 
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func calculateMid(remaining int) int {
-	return min(remaining-1, remaining/2)
-}
-
-func foundIndex(key int, array []int, offset int) int {
-	if array[offset] == key {
-		return offset
-	}
-	return -1
-}
-
 func chop(key int, array []int, offset, length int) int {
 	remaining := length - offset
-
 	if remaining == 0 {
 		return -1
 	}
-	if remaining == 1 {
-		return foundIndex(key, array, offset)
-	}
 
-	midPoint := calculateMid(remaining)
-
-	point := offset + midPoint
+	point := offset + remaining/2
 	if array[point] == key {
 		return point
 	}
